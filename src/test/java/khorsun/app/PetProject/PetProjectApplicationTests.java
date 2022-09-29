8df6960b06cc76ad1a255e242f424783e90c9e57
mishgan55 <vorota.mogilev@gmail.com>
2022-09-29 23:49:39 +0300

    Initial commit,start with spring boot initializer

---
+++ src/test/java/khorsun/app/PetProject/PetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package khorsun.app.PetProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
